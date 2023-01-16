02f1f5c2641ae611860124f877823e130967b716
lucasemanueldev <lucasemanuelroque2014@gmail.com>
2023-01-16 10:06:39 -0300

    modulo js

---
+++ Módulo 04 - JavaScript/0001/exercicio.js
@@ -0,0 +1,11 @@
+let NomeP1 = prompt("Digite o nome da pessoa mais velha:")
+let IdadeP1 = prompt("Digite a idade da pessoa mais velha:")
+
+let NomeP2 = prompt("Digite o nome da pessoa mais nova:")
+let IdadeP2 = prompt("Digite a idade da pessoa mais nova:")
+
+var diferenca = IdadeP1 - IdadeP2
+
+alert("A pessoa mais velha se chama " + NomeP1 + " e tem " + IdadeP1 + "anos\n")
+alert("A pessoa mais nova se chama " + NomeP2 + " e tem " + IdadeP2 + "anos\n")
+alert("A diferença de idade entre elas é: " + diferenca + " Anos")
---
+++ Módulo 04 - JavaScript/script.js
@@ -0,0 +1,8 @@
+alert("Bem Vindo! A seguir pediremos que informe alguns dados")
+
+var nome = prompt("Qual o seu nome?")
+
+let idade = prompt("Qual a sua idade?")
+let idade2 = confirm("Sua idade é " + idade + " anos?")
+
+alert("Nome: " + nome + "\nIdade: " + idade + "\nConfirmação da idade: " + idade2)
