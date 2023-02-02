5a012fae2dbe6021dbd7a2785b53e1781b9ccc09
Lucas Emanuel <88749179+lucasemanueldev@users.noreply.github.com>
2023-02-01 21:42:38 -0300

    Hello-React

---
+++ React/01/hello.js
@@ -0,0 +1,8 @@
+ReactDOM.render(
+    React.createElement(
+        'h1',
+        null,
+        'hello word'
+    ),
+    document.getElementById('root')
+)
---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWord from './hello';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWord/>
+    </div>
+  );
+}
+
+export default App;
