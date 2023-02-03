1c1a8814b8bf1b8de477b6d7b243d7ee62d82b62
Lucas Emanuel <88749179+lucasemanueldev@users.noreply.github.com>
2023-02-02 23:35:10 -0300

    FEV2

--- React/my-first-app/src/App.js
+++ React/my-first-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWord from './hello';
+import loremipsum from './components/loremipsum';
 
 function App() {
   return (
     <div className="App">
       <HelloWord/>
+      <loremipsum/>
     </div>
   );
 }
---
+++ React/my-first-app/src/components/loremipsum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const loremipsum = () => {
+    return  <p>eu sou mt lindo mds</p>
+}
+
+export default loremipsum;
